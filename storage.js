6e3d1e3c89df7565a30c2da11414e7c52936b48b
sansbobs <148600474+sansbobs@users.noreply.github.com>
2025-04-06 22:53:31 -0700

    Delete storage.js

--- storage.js
+++
@@ -1,9 +0,0 @@
-const STORAGE_KEY = 'adhd_web_checklists';
-
-export const getChecklists = () => {
-  return JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
-};
-
-export const saveChecklists = (data) => {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
-};
